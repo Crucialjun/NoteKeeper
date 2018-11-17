99958173fcc67c4b27f4a06f63c85d47c83df76c
crucialjun <nich.otieno@gmail.com>
2018-11-17 17:20:07 +0300

    Passing intents

--- app/src/main/java/com/example/notekeeper/NoteActivity.java
+++ app/src/main/java/com/example/notekeeper/NoteActivity.java
@@ -14,6 +14,7 @@ import android.widget.Spinner;
 import java.util.List;
 
 public class NoteActivity extends AppCompatActivity {
+    public static final String NOTE_INFO = "com.example.notekeeper";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
