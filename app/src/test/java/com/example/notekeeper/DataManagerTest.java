18fcf601647cf2d98351ce79de38a2bf16e3f350
Crucialjun <nich.otieno@gmail.com>
2018-12-13 14:49:11 +0300

    test Unit

--- app/src/test/java/com/example/notekeeper/DataManagerTest.java
+++ app/src/test/java/com/example/notekeeper/DataManagerTest.java
@@ -7,6 +7,25 @@ import static org.junit.Assert.*;
 public class DataManagerTest {
 
     @Test
-    public void createNewNote() {
+    public void createNewNote() throws Exception{
+        DataManager dataManager = DataManager.getInstance();
+        final CourseInfo course = dataManager.getCourse("android_async");
+        final String noteTitle = "Test note title";
+        final String noteText = "This is the body text of my test note";
+
+
+        int noteIndex = dataManager.createNewNote();
+        NoteInfo newNote = dataManager.getNotes().get(noteIndex);
+        newNote.setCourse(course);
+        newNote.setTitle(noteTitle);
+        newNote.setText(noteText);
+
+
+        NoteInfo compareNote = dataManager.getNotes().get(noteIndex);
+        assertSame(newNote,compareNote);
+        assertEquals(compareNote.getCourse(),course);
+        assertEquals(compareNote.getTitle(),noteTitle);
+        assertEquals(compareNote.getText(),noteText);
+
     }
 }
