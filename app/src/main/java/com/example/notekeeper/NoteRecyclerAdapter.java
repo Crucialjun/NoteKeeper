ccc2228c75121d2606e265dc88fcb617c7d4fe6a
Crucialjun <nich.otieno@gmail.com>
2019-01-06 13:30:54 +0300

    View Management

--- app/src/main/java/com/example/notekeeper/NoteRecyclerAdapter.java
+++ app/src/main/java/com/example/notekeeper/NoteRecyclerAdapter.java
@@ -4,11 +4,12 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-public class NoteRecyclerAdapter extends RecyclerView.Adapter<NoteRecyclerAdapter.ViewHolder{
-
+public class NoteRecyclerAdapter extends RecyclerView.Adapter<NoteRecyclerAdapter.ViewHolder>{
     private final Context mContext;
     private final LayoutInflater mLayoutInflater;
 
@@ -20,7 +21,8 @@ public class NoteRecyclerAdapter extends RecyclerView.Adapter<NoteRecyclerAdapte
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+        View itemView = mLayoutInflater.inflate(R.layout.item_note_list,viewGroup,false);
+        return new ViewHolder(itemView);
     }
 
     @Override
@@ -35,8 +37,14 @@ public class NoteRecyclerAdapter extends RecyclerView.Adapter<NoteRecyclerAdapte
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
+        public final TextView mTextCourse;
+        public final TextView mTextTitle;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            mTextCourse = itemView.findViewById(R.id.text_course);
+            mTextTitle = itemView.findViewById(R.id.text_title);
         }
     }
 }
