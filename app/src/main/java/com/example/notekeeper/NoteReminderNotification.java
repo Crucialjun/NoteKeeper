c7c32bf8993e0f4d65695163b3d979c1de3f09d8
crucialjun <nich.otieno@gmail.com>
2019-03-29 19:07:48 +0300

    Notification

--- app/src/main/java/com/example/notekeeper/NoteReminderNotification.java
+++ app/src/main/java/com/example/notekeeper/NoteReminderNotification.java
@@ -43,7 +43,7 @@ public class NoteReminderNotification {
      * @see #cancel(Context)
      */
     public static void notify(final Context context,
-                              final String exampleString, final int number) {
+                              final String noteText) {
         final Resources res = context.getResources();
 
         // This image is used as the notification's large icon (thumbnail).
@@ -51,11 +51,6 @@ public class NoteReminderNotification {
         final Bitmap picture = BitmapFactory.decodeResource(res, R.drawable.example_picture);
 
 
-        final String ticker = exampleString;
-        final String title = res.getString(
-                R.string.note_reminder_notification_title_template, exampleString);
-        final String text = res.getString(
-                R.string.note_reminder_notification_placeholder_text_template, exampleString);
 
         final NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
 
@@ -66,8 +61,8 @@ public class NoteReminderNotification {
                 // Set required fields, including the small icon, the
                 // notification title, and text.
                 .setSmallIcon(R.drawable.ic_stat_note_reminder)
-                .setContentTitle(title)
-                .setContentText(text)
+                .setContentTitle("Review Note")
+                .setContentText(noteText)
 
                 // All fields below this line are optional.
 
@@ -80,11 +75,10 @@ public class NoteReminderNotification {
                 .setLargeIcon(picture)
 
                 // Set ticker text (preview) information for this notification.
-                .setTicker(ticker)
+                .setTicker("Review Note")
 
                 // Show a number. This is useful when stacking notifications of
                 // a single type.
-                .setNumber(number)
 
                 // If this notification relates to a past or upcoming event, you
                 // should set the relevant time information using the setWhen
